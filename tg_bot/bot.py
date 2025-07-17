ad6c061484170af91e8a67e13e6702ede5be5db1
Varenik-vkusny <akybaevtimur7@gmail.com>
2025-07-17 15:59:38 +0500

    Add auth logic to bot

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -20,9 +20,15 @@ load_dotenv(dotenv_path=env_path)
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_BASE_URL = os.getenv('API_BASE_URL')
+users_token = {}
 
 
 class RegisterStates(StatesGroup):
+    waiting_for_username = State()
+    waiting_for_password = State()
+
+
+class LoginStates(StatesGroup):
     waiting_for_username = State()
     waiting_for_password = State()
 
@@ -65,7 +71,7 @@ async def register_username_handler(message: types.Message, state: FSMContext):
     
     await state.update_data(username=message.text)
 
-    await message.answer('Отлично! Теперь введите пароль:')
+    await message.answer('Отлично! Теперь введите пароль (минимум 3 символа):')
 
     await state.set_state(RegisterStates.waiting_for_password)
 
@@ -73,8 +79,12 @@ async def register_username_handler(message: types.Message, state: FSMContext):
 @router.message(RegisterStates.waiting_for_password)
 async def register_password_handler(message: types.Message, state: FSMContext):
 
+    if len(message.text) < 3:
+        await message.answer('Ваш пароль слишком маленький! Попробуйте еще раз.')
+        return
+
     user_data = await state.get_data()
-    username = user_data.get['username']
+    username = user_data.get('username')
     password = message.text
 
     register_data = {
@@ -94,4 +104,52 @@ async def register_password_handler(message: types.Message, state: FSMContext):
                 await message.answer(f'Регистрация прошла успешно! Ваш id: {new_user['id']}, ваш ник: {new_user['username']}')
         except httpx.RequestError:
             await message.answer('не удалось подключиться к серверу')
+    await state.clear()
+
+
+@router.message(F.text == 'Войти')
+async def start_login(message: types.Message, state: FSMContext):
+    await message.answer('Давайте начнем процесс аутентификации! Введите ваш ник: ')
+
+    await state.set_state(LoginStates.waiting_for_username)
+
+
+@router.message(LoginStates.waiting_for_username)
+async def login_username_handler(message: types.Message, state: FSMContext):
+    await state.update_data(username=message.text)
+
+    await message.answer('Отлично! Теперь введите ваш пароль:')
+
+    await state.set_state(LoginStates.waiting_for_password)
+
+
+@router.message(LoginStates.waiting_for_password)
+async def login_password_handler(message: types.Message, state: FSMContext):
+    
+    user_data = await state.get_data()
+    username = user_data.get('username')
+    password = message.text()
+
+    login_data = {
+        'username': username,
+        'password': password
+    }
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(f'{API_BASE_URL}/token', data=login_data)
+
+            if response.status_code != 201:
+                error_detail = response.json().get('detail', 'Неправильный ник или пароль')
+                await message.answer(f'Произошла ошибка: {error_detail}')
+                return
+            
+            token_data = response.json()
+            access_token = token_data.get('access_token')
+            user_id = message.from_user.id
+            users_token[user_id] = access_token
+
+            await message.answer('Вы успешно авторизованы!')
+        except httpx.RequestError:
+            await message.answer('Не удалось подключиться к серверу.')
     await state.clear()
